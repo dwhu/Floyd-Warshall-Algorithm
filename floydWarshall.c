784f116b4edbcc26a9ad4bc0b772babc9628958c
David Hughes <hughdw11@wfu.edu>
2015-04-27 16:55:16 -0400

    Working communication Pattern

--- floydWarshall.c
+++ floydWarshall.c
@@ -65,11 +65,16 @@ int main(int argc, char* argv[]){
     /* Local Info */
     int world_id;
     int i,j;
+    int index;
     int p;
     int n;
     int nn;
     int nnp;
     int n_sqp;
+    int starting_i;
+    int ending_i;
+    int starting_j;
+    int ending_j;
     double * local_section;
     double * row_section;
     double * column_section;
@@ -133,6 +138,7 @@ int main(int argc, char* argv[]){
     // Find the workers in my column
     for(i =0; i < sqroot_p; i++){
         my_column_group[i] = i*sqroot_p + world_id %sqroot_p;
+
     }
     /*Create the group and communication port*/
     MPI_Group_incl(world_group,sqroot_p,my_column_group,&column_group);
@@ -187,7 +193,7 @@ int main(int argc, char* argv[]){
     MPI_Barrier(MPI_COMM_WORLD);
 
     /***************************************************
-                End Read in and Distribute
+                End Read In to Master Worker
     ***************************************************/
 
     /* Tell Everyone what n is */
@@ -197,10 +203,9 @@ int main(int argc, char* argv[]){
     n_sqp = n / sqroot_p;
 
 
-    //Distribute local sections
-    // local_section = (double*) safe_malloc("creating local_section buffer",nnp*sizeof(double));
-    // MPI_Scatter((void *) map, nnp, MPI_DOUBLE, (void *) local_section, nnp, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
+    /***************************************************
+                Distribute Blocks to Workers
+    ***************************************************/
     int dest;
     for(dest = p-1; dest >= 0; dest--){
 
@@ -208,18 +213,15 @@ int main(int argc, char* argv[]){
         if(world_id == 0){
             local_section = (double*) safe_malloc("creating local_section buffer",nnp*sizeof(double));
 
-            int starting_i = (dest % sqroot_p)*n_sqp;
-            int ending_i = starting_i+n_sqp;
-            int starting_j = (dest / sqroot_p)*n_sqp;
-            int ending_j = starting_j+n_sqp;
+            starting_i = (dest % sqroot_p)*n_sqp;
+            ending_i = starting_i+n_sqp;
+            starting_j = (dest / sqroot_p)*n_sqp;
+            ending_j = starting_j+n_sqp;
 
-            printf("dest(%d) %d <= i < %d %d <= j < %d\n",dest,starting_i,ending_i,starting_j,ending_j);
-
-            int index = 0;
+            index = 0;
             for(j = starting_j; j < ending_j;j++){
                 int j_index = j*n;
                 for(i = starting_i;i < ending_i; i++){
-                    // local_section[index] = ;
                     local_section[index] = map[i+j_index];
                     index++;
                 }
@@ -227,112 +229,168 @@ int main(int argc, char* argv[]){
 
             if(dest != 0){
                 MPI_Send(local_section,nnp,MPI_DOUBLE,dest,dest,MPI_COMM_WORLD);
+            }else{
+                printf("(%d)\n",world_id);
+                for(j=0;j<n_sqp;j++){
+                    for(i=0;i<n_sqp;i++){
+                        printf(" %f ", local_section[i+j*n_sqp]);
+                    }
+                    printf("\n");
+                }
+                printf("\n\n");
             }
 
         }else if( dest == world_id){
 
             local_section = (double*) safe_malloc("creating local_section buffer",nnp*sizeof(double));
             MPI_Recv(local_section,nnp,MPI_DOUBLE,0,dest,MPI_COMM_WORLD,&status);
-        }
 
-        MPI_Barrier(MPI_COMM_WORLD);
-    }
-
-    for(i = 0; i < p; i++){
-        if(i == world_id){
-            printf("(%d):",world_id);
-            int x;
-            for(x =0; x< nnp; x++){
-                if(x % n_sqp == 0){
-                    printf("\n");
+            printf("(%d)\n",world_id);
+            for(j=0;j<n_sqp;j++){
+                for(i=0;i<n_sqp;i++){
+                    printf(" %f ", local_section[i+j*n_sqp]);
                 }
-
-                printf(" %f ",local_section[x]);
+                printf("\n");
             }
             printf("\n\n");
         }
+
         MPI_Barrier(MPI_COMM_WORLD);
     }
-
+    
     int row_block = world_id/sqroot_p;
-    int column_block = world_group & sqroot_p;
+    int column_block = world_id % sqroot_p;
     int kb,kc;
 
     //Iteration Block from 0 to root(p)
     for(kb=0;kb<sqroot_p;kb++){
+
+        if(world_id == 0){
+            printf("\nKB = %d\n\n",kb);
+        }
+
         //Iteration over a column/row workers partial segment block
         //i to n/root(p)
         for(kc=0;kc<n_sqp;kc++){
 
+            MPI_Barrier(MPI_COMM_WORLD);
 
             /****************************************
-            ********* Distirubte the Row and Columns
+                  Distirubte the Row and Columns
             *****************************************/
 
             //Allocate Memory
             row_section = (double *) safe_malloc("creating buffer", n_sqp*sizeof(double));
-            column_section = (double *) safe_malloc("creating buffer", n_sqp*sizeof(double));
 
             //if this worker holds the row section
             if(row_block == kb){
+                
                 //Copy the Data in
+                int target_row = kc*n_sqp;
                 for(i=0; i < n_sqp; i++){
-                    row_section[i] = local_section[i+kc*n_sqp];
+                    row_section[i] = local_section[i+target_row];
                 }
             }
+            MPI_Bcast(row_section,n_sqp,MPI_DOUBLE,kb,column_comm);
+            MPI_Barrier(MPI_COMM_WORLD);
+
+            column_section = (double *) safe_malloc("creating buffer", n_sqp*sizeof(double));
 
             //if this worker holds this column section
-            if(column_block == kb){
+            if(kb == column_block){
+
                 //Copy the data in
-                for(i=0; i < n_sqp; i++){
-                    column_section[i] = local_section[kc+i*n_sqp];
+                for(j=0; j < n_sqp; j++){
+                    int row = j*n_sqp;
+                    column_section[j] = local_section[kc + row];
+
                 }
             }
-
-            MPI_Barrier(MPI_COMM_WORLD);
 
             //Broadcast both row and column to the correct group
-            MPI_Bcast(&row_section,n_sqp,MPI_DOUBLE,kb,column_comm);
-            MPI_Bcast(&column_section,n_sqp,MPI_DOUBLE,kb,row_comm);
-
+            MPI_Bcast(column_section,n_sqp,MPI_DOUBLE,kb,row_comm);
             MPI_Barrier(MPI_COMM_WORLD);
-
-            if(world_id == 0){
-                printf("Row and Column Distirubted\nColumn Section: ");
-
-                for(i =0;i< n_sqp;i++){
-                    printf(" %f ", column_section[i]);
-                }
-
-                printf("\nRow Section: ");
-
-                 for(i =0;i< n_sqp;i++){
-                    printf(" %f ", row_section[i]);
-                }
-
-            }
 
             //Update
             for(i =0; i < n_sqp; i++){
                 for(j=0;j< n_sqp;j++){
-                    if(row_section[i]+column_section[j] < local_section[i*n_sqp+j]){
+                    if(row_section[i]+column_section[j] < local_section[i*n_sqp+j] && row_section[i]+column_section[j] > 0){
                         local_section[i*n_sqp+j] = row_section[i]+column_section[j];
                     }
                 }
             }
+            MPI_Barrier(MPI_COMM_WORLD);
+
+
+            if(world_id == 0){
+                printf("KC = %d\n",kc);
+             }
 
             //Free the row and Column after done
             free((void *) column_section);
             free((void *) row_section);
 
-            MPI_Barrier(MPI_COMM_WORLD);
         }
     }
 
+    if(world_id == 0){
+        index = 0;
+        for(j=0;j<n_sqp;j++){
+            for(i=0;i<n_sqp;i++){
+                map[index] = local_section[i+j*n_sqp];
+                index++;
+            }
+        }
+    }
 
+    for(dest = 1; dest < p; dest++){
 
-    free((void *) map);
-    free((void *) local_section);
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        /*****************************
+            Send Data Back to Master
+        ******************************/
+        if(world_id == dest){
+
+            MPI_Send(local_section,nnp,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+
+        }else if(world_id == 0){
+
+            /******************************************
+                Master Recieves Data and Puts it back
+             ******************************************/
+            local_section = (double*) safe_malloc("creating local_section buffer",nnp*sizeof(double));
+            MPI_Recv(local_section,nnp,MPI_DOUBLE,dest,0,MPI_COMM_WORLD,&status);
+
+            starting_i = (dest % sqroot_p)*n_sqp;
+            ending_i = starting_i+n_sqp;
+            starting_j = (dest / sqroot_p)*n_sqp;
+            ending_j = starting_j+n_sqp;
+
+            index = 0;
+            for(j = starting_j; j < ending_j;j++){
+                int j_index = j*n;
+                for(i = starting_i;i < ending_i; i++){
+                    map[i+j_index] = local_section[index];
+                    index++;
+                }
+            }
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if(world_id==0){
+        printf("\n\n");
+        for(j=0;j<n;j++){
+            for(i=0;i<n;i++){
+                printf(" %f ",map[i+j*n_sqp]);
+            }
+            printf("\n");
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
     MPI_Finalize();
     
 }
